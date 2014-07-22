46372efb636182353ebd7692bc8160f3724be043
Matthieu NICOLAS <matthieu.nicolas@telecomnancy.net>
2014-07-22 16:25:59 +0200

    Update the username's module

--- assets/js/awareness-modules/username.js
+++ assets/js/awareness-modules/username.js
@@ -7,16 +7,60 @@
         */
 
         var item = this;
+        var timeoutUpdateUsername = null;
 
         var updateUsername = function (username) {
             item.val($('<div/>').html(username).text());
         };
+
+        var changeUsername = function () {
+            // Encode the username to avoid some Javascript injections
+            var username = $('<div/>').text(item.val()).html().trim();
+            var regexp = /^[a-zA-Z0-9,]+(\s{0,1}[a-zA-Z0-9,])*$/
+            var title = '';
+            item.tooltip('destroy');
+            if(username.length > 0 && username.length <= 20 && username.match(regexp) !== null) {
+                title = 'Username updated!';
+                if(timeoutUpdateUsername === null) {
+                    timeoutUpdateUsername = setTimeout(function () {        
+                        options.infosUsersModule.updateLocalUsername(username);
+                        clearTimeout(timeoutUpdateUsername);
+                        timeoutUpdateUsername = null;
+                    }, 3000);  
+                }
+            }
+            else if(username.length === 0) {
+                title = 'The username can\'t be empty...';
+            }
+            else if(username.length > 20) {
+                title = 'Too long... Choose a shorter username!';
+            }
+            else if(username.match(regexp) === null) {
+                title = 'Please use only alphanumerics characters!';
+            }
+            item.tooltip({
+                title: title,
+                placement: 'bottom',
+                trigger: 'manual'
+            }).tooltip('show');
+            setTimeout(function () {
+                item.tooltip('hide');
+            }, 3000);
+        };
+
 
         options.infosUsersModule.on('changeLocalUsername', function (data) {
             updateUsername(data.username);
         });
 
         updateUsername('Initializing...');
+
+        this.keypress(function(e) {
+                // Enter pressed?
+                if(e.which === 10 || e.which === 13) {
+                    changeUsername();
+                }
+         });
 
         return this;
     };
